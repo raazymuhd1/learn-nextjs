d45a3f28604eae864ae31396cbd4073f9b6249f7
Muhammad Razi <65342312+muhammadraazy@users.noreply.github.com>
2022-09-22 01:37:41 +0700

    CSR Vs SSR Vs SSG
    adding a client-side rendering, server-side rendering, and static side generation definition

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import { useSession } from "next-auth/client"
 
+// Client-side rendering => rendering/fetching the content after the page loaded
+// Server-side rendering => rendering/fetching the content on the page load
+// Static-side Generation => rendering/fetching the content the content load
+
 export default function Home() {
          const dude = "dude"
          const [ session, loading ] = useSession()
